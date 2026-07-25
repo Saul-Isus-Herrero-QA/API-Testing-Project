8cbc8b10e65c4dfa74952a153de32e07cc8041d7
Saul_QA <testst762@gmail.com>
2026-07-25 16:22:56 +0200

    Update CookiesManagerTest.java

--- src/test/java/tests/CookiesManagerTest.java
+++ src/test/java/tests/CookiesManagerTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.time.LocalDateTime;
 import utils.CookiesManager;
+import io.qameta.allure.*;
 
 public class CookiesManagerTest {
 
